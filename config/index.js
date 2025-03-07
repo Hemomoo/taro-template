8c128927501e32218d3f598aaa6ef1e5c701ed9f
邓亮 <787615673@qq.com>
2025-03-07 15:45:05 +0800

    feat: 新增 h5配置

--- config/index.js
+++ config/index.js
@@ -82,6 +82,27 @@ const config = {
       enable: true,
     },
   },
+  h5:{
+    esnextModules: [/@antmjs[\/]vantui/],
+    postcss: {
+      autoprefixer: {
+        enable: true,
+        config: {
+        }
+      },
+      pxtransform: {
+        enable: true,
+        config: {},
+      },
+      cssModules: {
+        enable: false, // 默认为 false，如需使用 css modules 功能，则设为 true
+        config: {
+          namingPattern: 'module', // 转换模式，取值为 global/module
+          generateScopedName: '[name]__[local]___[hash:base64:5]'
+        }
+      }
+    }
+  },
   plugins: [["@tarojs/plugin-framework-react", { reactMode: "concurrent" }]],
 };
 
--- src/hooks/useCheckUpdate.ts
+++ src/hooks/useCheckUpdate.ts
@@ -6,42 +6,42 @@ import Taro, { useDidShow } from "@tarojs/taro";
  */
 const useCheckUpdate = () => {
   // 获取小程序更新管理器
-  const updateManager = Taro.getUpdateManager();
+  // const updateManager = Taro.getUpdateManager();
 
   // 在组件显示时执行更新检查和处理
   useDidShow(() => {
     // 确保在下一个事件循环中执行更新检查逻辑
-    Taro.nextTick(() => {
-      // 监听更新检查结果
-      updateManager.onCheckForUpdate((res) => {
-        res.hasUpdate && console.log("---小程序有更新版本 🚀---");
-      });
+    // Taro.nextTick(() => {
+    //   // 监听更新检查结果
+    //   updateManager.onCheckForUpdate((res) => {
+    //     res.hasUpdate && console.log("---小程序有更新版本 🚀---");
+    //   });
 
-      // 监听版本更新准备就绪事件
-      updateManager.onUpdateReady(() => {
-        // 显示模态框提示用户更新
-        Taro.showModal({
-          title: "更新提示",
-          content: "新版本已经准备好，是否重启应用？",
-          showCancel: false,
-          success: (res) => {
-            if (res.confirm) {
-              // 新版本已经下载好，调用 applyUpdate 应用新版本并重启
-              updateManager.applyUpdate();
-            }
-          },
-        });
-      });
+    //   // 监听版本更新准备就绪事件
+    //   updateManager.onUpdateReady(() => {
+    //     // 显示模态框提示用户更新
+    //     Taro.showModal({
+    //       title: "更新提示",
+    //       content: "新版本已经准备好，是否重启应用？",
+    //       showCancel: false,
+    //       success: (res) => {
+    //         if (res.confirm) {
+    //           // 新版本已经下载好，调用 applyUpdate 应用新版本并重启
+    //           updateManager.applyUpdate();
+    //         }
+    //       },
+    //     });
+    //   });
 
-      // 监听版本更新失败事件
-      updateManager.onUpdateFailed(() => {
-        // 新版本下载失败，提示重新打开
-        Taro.showModal({
-          title: "更新失败",
-          content: "请删除小程序后重新打开",
-        });
-      });
-    });
+    //   // 监听版本更新失败事件
+    //   updateManager.onUpdateFailed(() => {
+    //     // 新版本下载失败，提示重新打开
+    //     Taro.showModal({
+    //       title: "更新失败",
+    //       content: "请删除小程序后重新打开",
+    //     });
+    //   });
+    // });
   });
 };
 
